95ac10ed7885e54f04704eaf5470d6c7eb597d24
Adriano <adriano.cesar@n-able.com>
2025-04-27 18:39:15 +0100

    feat: add ngrx boilerplate

--- apps/towersVisualization/src/app/app.config.ts
+++ apps/towersVisualization/src/app/app.config.ts
@@ -8,19 +8,20 @@ import { appRoutes } from './app.routes';
 import { provideStore, provideState } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
-import * as fromTowers from './+state/towers.reducer';
 import { TowersEffects } from './+state/towers.effects';
 import { TowersFacade } from './+state/towers.facade';
+import { TOWERS_FEATURE_KEY, towersReducer } from './+state/towers.reducer';
+import { TowerService } from './simulation/tower.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideEffects(TowersEffects),
-    provideState(fromTowers.TOWERS_FEATURE_KEY, fromTowers.towersReducer),
+    provideState(TOWERS_FEATURE_KEY, towersReducer),
     TowersFacade,
-    provideStore(),
+    provideStore({ towers: towersReducer }),
     provideStoreDevtools({ logOnly: !isDevMode() }),
-    provideEffects(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
+    TowerService,
   ],
 };
