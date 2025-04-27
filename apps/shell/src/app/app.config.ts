4d4fb7391d50871c5aa398773bcd8e9075e903a5
Adriano <adriano.cesar@n-able.com>
2025-04-27 19:12:11 +0100

    not working: share store between mfe and shell

--- apps/shell/src/app/app.config.ts
+++ apps/shell/src/app/app.config.ts
@@ -1,10 +1,15 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { provideState, provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
+    provideEffects([]),
+    provideState('',{}),
+    provideStore(),
   ],
 };
--- apps/towersVisualization/module-federation.config.ts
+++ apps/towersVisualization/module-federation.config.ts
@@ -4,9 +4,11 @@ const config: ModuleFederationConfig = {
   name: 'towersVisualization',
   exposes: {
     './Routes': 'apps/towersVisualization/src/app/remote-entry/entry.routes.ts',
+    './AppConfig': './apps/towersVisualization/src/app/app.config.ts',
   },
-};
 
+};
+//'./AppConfig': './src/app/app.config.ts',
 /**
  * Nx requires a default export of the config to allow correct resolution of the module federation graph.
  **/
